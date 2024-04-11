fc8c8bce4c5e122b549571ce4185d8b6ba858528
Moein Arabi <moein.mo81@gmail.com>
2024-04-11 19:58:08 +0330

    Add if-else statements

--- code/AST.h
+++ code/AST.h
@@ -22,23 +22,121 @@ class AssignStatement;          // assignment statement like a = 3;
 class CommentStatement;
 class afterCheckStatement;
 
-class ElseIfStatement : public Statement
+// stores information of a statement. For example x=56; is a statement
+class Statement : public TopLevelEntity
 {
+public:
+    enum StatementType
+    {
+        If,
+        ElseIf,
+        Else
+    };
+
 private:
-    Expression *Condition;
-    llvm::SmallVector<Statement *> Statements;
+    StatementType Type;
 
 public:
-    ElifStatement(Expression *condition, llvm::SmallVector<Statement *> statements, StateMentType type) : Condition(condition), Statements(statements), Statement(type) {}
+    StatementType getKind()
+    {
+        return Type;
+    }
+
+    Statement(StatementType type) : Type(type) {}
+    virtual void accept(ASTVisitor &V) override
+    {
+        V.visit(*this);
+    }
+};
+
+class IfStatement : public Statement
+{
+
+private:
+    Expression *condition;
+    llvm::SmallVector<Statement *> statements;
+    llvm::SmallVector<ElseIfStatement *> elseIfStatements;
+    ElseStatement *elseStatement;
+    bool hasElseIf;
+    bool hasElse;
+
+public:
+    IfStatement(Expression *condition,
+                llvm::SmallVector<Statement *> statements,
+                llvm::SmallVector<ElseIfStatement *> elseIfStatements,
+                ElseStatement *elseStatement,
+                bool hasElseIf,
+                bool hasElse,
+                StatementType type) : condition(condition),
+                                      statements(statements),
+                                      elseIfStatements(elseIfStatements),
+                                      elseStatement(elseStatement),
+                                      hasElseIf(hasElseIf),
+                                      hasElse(hasElse),
+                                      Statement(type) {}
 
     Expression *getCondition()
     {
-        return Condition;
+        return condition;
+    }
+
+    bool HasElseIf()
+    {
+        return hasElseIf;
+    }
+
+    bool HasElse()
+    {
+        return hasElse;
+    }
+
+    llvm::SmallVector<ElseIfStatement *> getElseIfStatements()
+    {
+        return elseIfStatements;
     }
 
     llvm::SmallVector<Statement *> getStatements()
     {
-        return Statements;
+        return statements;
+    }
+
+    ElseStatement *getElseStatement()
+    {
+        return elseStatement;
+    }
+
+    virtual void accept(ASTVisitor &V) override
+    {
+        V.visit(*this);
+    }
+};
+
+class ElseIfStatement
+{
+private:
+    IfStatement *ifStatement;
+
+public:
+    ElseIfStatement(IfStatement *ifStatement) : ifStatement(ifStatement) {}
+
+    IfStatement *getIfStatement()
+    {
+        return ifStatement;
+    }
+};
+
+class ElseStatement : public Statement
+{
+
+private:
+    llvm::SmallVector<Statement *> statements;
+
+public:
+    ElseStatement(llvm::SmallVector<Statement *> statements, StatementType type) : statements(statements), Statement(type) {}
+
+    llvm::SmallVector<Statement *> getStatements()
+    {
+        return statements;
     }
 
     virtual void accept(ASTVisitor &V) override
