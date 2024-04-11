1205e56d873c6bf385c02d2d68fc1f42bc4d6939
Moein Arabi <moein.mo81@gmail.com>
2024-04-11 20:18:09 +0330

    Add if parser

--- code/parser.cpp
+++ code/parser.cpp
@@ -9,3 +9,68 @@ Base *Parser::parse()
     Base *Res = parseS();
     return Res;
 }
+
+IfStatement *Parser::parseIf()
+{
+    advance();
+    if (!Tok.is(Token::l_paren))
+    {
+        Error::LeftParenthesisExpected();
+    }
+
+    advance();
+    Expression *condition = parseLogicalValue();
+
+    if (!Tok.is(Token::r_paren))
+    {
+        Error::RightParenthesisExpected();
+    }
+
+    advance();
+    if (!Tok.is(Token::l_brace))
+    {
+        Error::LeftBraceExpected();
+    }
+    Base *allIfStatements = parseStatement();
+    if (!Tok.is(Token::r_brace))
+    {
+        Error::RightBraceExpected();
+    }
+    advance();
+
+    // parse else if and else statements
+    llvm::SmallVector<ElseIfStatement *> elseIfStatements;
+    ElseStatement *elseStatement = nullptr;
+    bool hasElseIf = false;
+    bool hasElse = false;
+
+    while (Tok.is(Token::KW_else))
+    {
+        advance();
+        if (Tok.is(Token::KW_if))
+        {
+            IfStatement *ifStatement = parseIf();
+            elseIfStatements.push_back(new ElseIfStatement(ifStatement));
+            hasElseIf = true;
+        }
+        else if (Tok.is(Token::l_brace))
+        {
+            advance();
+            Base *allElseStatements = parseStatement();
+            if (!Tok.is(Token::r_brace))
+            {
+                Error::RightBraceExpected();
+            }
+            advance();
+            elseStatement = new ElseStatement(allElseStatements, Statement::StatementType::Else);
+            hasElse = true;
+            break;
+        }
+        else
+        {
+            Error::RightBraceExpected();
+        }
+    }
+
+    return new IfStatement(condition, allIfStatements->getStatements(), elseIfStatements, elseStatement, hasElseIf, hasElse, Statement::StatementType::If);
+}
