190f2561e410f214ff285e414b3e562e79e1c7bc
Moein Arabi <moein.mo81@gmail.com>
2024-04-13 15:31:59 +0330

    Add semantic.cpp

---
+++ code/semantic.cpp
@@ -0,0 +1,131 @@
+#include "semantic.h"
+#include "llvm/ADT/StringSet.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace
+{
+    class DeclCheck : public ASTVisitor
+    {
+        llvm::StringSet<> Scope;
+        bool HasError;
+
+        enum ErrorType
+        {
+            Twice,
+            Not,
+            DivByZero
+        };
+
+        void error(ErrorType ET, llvm::StringRef V)
+        {
+            if (ET == ErrorType::DivByZero)
+            {
+                llvm::errs() << "Division by zero is not allowed."
+                             << "\n";
+            }
+            else
+            {
+                llvm::errs() << "Variable " << V << " is " << (ET == Twice ? "already" : "not") << " declared!\n";
+            }
+            HasError = true;
+            exit(3);
+        }
+
+    public:
+        DeclCheck() : HasError(false) {}
+
+        bool hasError() { return HasError; }
+
+        virtual void visit(Base &Node) override{
+            // TODO: Implement
+        };
+
+        virtual void visit(Unary &Node) override{
+            // TODO: Implement
+        };
+
+        virtual void visit(PrintStatement &Node) override{
+            // TODO: Implement
+        };
+
+        virtual void visit(BinaryOp &Node) override{
+            // TODO: Implement
+        };
+
+        virtual void visit(Statement &Node) override{
+            // TODO: Implement
+
+        };
+
+        virtual void visit(BooleanOp &Node) override{
+            // TODO: Implement
+
+        };
+
+        virtual void visit(Expression &Node) override{
+            // TODO: Implement
+        };
+
+        virtual void visit(DecStatement &Node) override{
+            // TODO: Implement
+        };
+
+        virtual void visit(IfStatement &Node) override
+        {
+
+            Expression *declaration = (Expression *)Node.getCondition();
+            declaration->accept(*this);
+            llvm::SmallVector<Statement *> stmts = Node.getStatements();
+            for (auto I = stmts.begin(), E = stmts.end(); I != E; ++I)
+            {
+                (*I)->accept(*this);
+            }
+        };
+
+        virtual void visit(ElifStatement &Node) override
+        {
+
+            Expression *declaration = (Expression *)Node.getCondition();
+            declaration->accept(*this);
+            llvm::SmallVector<Statement *> stmts = Node.getStatements();
+            for (auto I = stmts.begin(), E = stmts.end(); I != E; ++I)
+            {
+                (*I)->accept(*this);
+            }
+        };
+
+        virtual void visit(ElseStatement &Node) override
+        {
+
+            llvm::SmallVector<Statement *> stmts = Node.getStatements();
+            for (auto I = stmts.begin(), E = stmts.end(); I != E; ++I)
+            {
+                (*I)->accept(*this);
+            }
+        };
+
+        virtual void visit(LoopStatement &Node) override{
+            // TODO: Implement
+
+        };
+
+        virtual void visit(AssignStatement &Node) override{
+            // TODO: Implement
+
+        };
+
+        virtual void visit(CommentStatement &Node) override{
+            // TODO: Implement
+
+        };
+    };
+}
+
+bool Semantic::semantic(AST *Tree)
+{
+    if (!Tree)
+        return false;
+    DeclCheck Check;
+    Tree->accept(Check);
+    return Check.hasError();
+}
