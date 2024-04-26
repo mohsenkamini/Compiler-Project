93d1c5cec962e6115ba72efadfe985238cd8e3a2
mohammad nakhjiri <mohammaddev02@gmail.com>
2024-04-26 10:21:49 +0330

    fixing print of bool

--- project_lib.c
+++ project_lib.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// void print(int v)
-// {
-//     // check if bool or not
-//     if (v == 1)
-//         printf("1\n");
-//     else if (v == 0)
-//         printf("0\n");
-//     else
-//         printf("%d\n", v);
-// }
-
-void print(void *v){
-    int val = *(int*)v;
-    printf("%d\n", *(int*)v);
+void print(int v){
+    printf("%d\n", v);
 }
