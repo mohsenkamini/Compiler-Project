0e113e99e980cb7e69c2208679455fd6ae29058a
mohammad nakhjiri <mohammaddev02@gmail.com>
2024-04-26 17:05:19 +0330

    skipping warnings

--- makeRun.sh
+++ makeRun.sh
@@ -4,11 +4,11 @@
 cd build/code/
 ./compiler "$(cat ../../input.txt)" > compiler.ll
 
-# Step 2: Compile the support library to an object file
-clang -c ../../project_lib.c -o lib.o
+# Step 2: Compile the support library to an object file, suppressing warnings
+clang -w -c ../../project_lib.c -o lib.o
 
-# Step 3: Compile the LLVM IR to an object file
-clang -c compiler.ll -o compiler.o
+# Step 3: Compile the LLVM IR to an object file, suppressing warnings
+clang -w -c compiler.ll -o compiler.o
 
 # Step 4: Link the object files to create the executable
 clang compiler.o lib.o -o executable
