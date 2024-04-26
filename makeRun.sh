c385fb8a35df0399b390c5a8feb8bbe8fcc6bbf2
mohammad nakhjiri <mohammaddev02@gmail.com>
2024-04-26 10:51:00 +0330

    debugging

--- makeRun.sh
+++ makeRun.sh
@@ -5,7 +5,7 @@ cd build/code/
 ./compiler "$(cat ../../input.txt)" > compiler.ll
 
 # Step 2: Compile the support library to an object file
-clang++ -c ../../project_lib.cpp -o lib.o
+clang -c ../../project_lib.c -o lib.o
 
 # Step 3: Compile the LLVM IR to an object file
 clang -c compiler.ll -o compiler.o
