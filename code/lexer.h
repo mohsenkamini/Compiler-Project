def784f22ad707aecbca7de9ebda861d7a8d0e2b
Am8xin <135750826+Am8xin@users.noreply.github.com>
2025-04-22 21:17:20 +0330

    Update lexer.h

--- code/lexer.h
+++ code/lexer.h
@@ -4,99 +4,104 @@
 #include "llvm/Support/MemoryBuffer.h"
 #include <string>
 
-
 class Lexer;
 
-class Token
-{
+class Token {
     friend class Lexer;
 
 public:
-    enum TokenKind : unsigned short
-    {
-        semi_colon,    // ;
-        unknown,       // unknown token
-        identifier,    // identifier like a, b, c, d, etc.
-        number,        // number like 1, 2, 3, 4, etc.
-        comma,         // ,
-        plus,          // +
-        minus,         // -
-        star,          // *
-        mod,           // %
-        slash,         // /
-        power,         // ^
-        l_paren,       // (
-        r_paren,       // )
-        l_brace,       // {
-        r_brace,       // }
-        plus_equal,    // +=
-        plus_plus,     // ++
-        minus_minus,   // --
-        minus_equal,   // -=
-        star_equal,    // *=
-        mod_equal,     // %=
-        slash_equal,   // /=
-        equal,         // =
-        equal_equal,   // ==
-        not_equal,     // !=
-        KW_not,           // !
-        less,          // <
-        less_equal,    // <=
-        greater,       // >
-        greater_equal, // >=
-        comment,       // /*
-        uncomment,     // */
-        KW_int,        // int
-        KW_bool,       // bool
-        KW_if,         // if
-        KW_else,       // else
-        KW_while,      // while
-        KW_for,        // for
-        KW_and,        // and
-        KW_or,         // or
-        KW_true,       // true
-        KW_false,      // false
-        KW_print,      // print
-        eof            // end of file
+    enum TokenKind : unsigned short {
+        // پایه
+        semi_colon,      // ;
+        unknown,         // ناشناخته
+        identifier,      // شناسه
+        number,          // عدد
+        comma,           // ,
+        l_paren,         // (
+        r_paren,         // )
+        l_brace,         // {
+        r_brace,         // }
+        eof,             // پایان فایل
+        
+        // عملگرها
+        plus,            // +
+        minus,           // -
+        star,            // *
+        mod,             // %
+        slash,           // /
+        power,           // ^
+        plus_equal,      // +=
+        minus_equal,     // -=
+        star_equal,      // *=
+        mod_equal,       // %=
+        slash_equal,     // /=
+        equal,           // =
+        equal_equal,     // ==
+        not_equal,       // !=
+        less,            // <
+        less_equal,      // <=
+        greater,         // >
+        greater_equal,   // >=
+        
+        // کلمات کلیدی
+        KW_int,          // int
+        KW_bool,         // bool
+        KW_float,        // float
+        KW_char,         // char
+        KW_string,       // string
+        KW_array,        // array
+        KW_if,           // if
+        KW_else,         // else
+        KW_while,        // while
+        KW_for,          // for
+        KW_and,          // and
+        KW_or,           // or
+        KW_true,         // true
+        KW_false,        // false
+        KW_print,        // print
+        KW_not,          // !
+        
+        // توکنهای خاص
+        comment_start,   /* 
+        comment_end,     */
+        increment,       // ++
+        decrement        // --
     };
 
 private:
-    TokenKind Kind;       // <type of token>
-    llvm::StringRef Text; // <token context>
+    TokenKind Kind;          // نوع توکن
+    llvm::StringRef Text;    // متن توکن
 
 public:
     TokenKind getKind() const { return Kind; }
     bool is(TokenKind K) const { return Kind == K; }
     llvm::StringRef getText() const { return Text; }
 
-    // kind="+" isOneOf(plus, minus) -> true
-    bool isOneOf(TokenKind K1, TokenKind K2) const
-    {
-        return is(K1) || is(K2);
+    template <typename... Ts>
+    bool isOneOf(TokenKind K1, Ts... Ks) const {
+        return is(K1) || isOneOf(Ks...);
     }
 
-    template <typename... Ts> // variadic template can take several inputs
-    bool isOneOf(TokenKind K1, TokenKind K2, Ts... Ks) const
-    {
-        return is(K1) || isOneOf(K2, Ks...);
+    bool isOneOf(TokenKind K1, TokenKind K2) const {
+        return is(K1) || is(K2);
     }
 };
 
-class Lexer
-{
+class Lexer {
     const char *BufferStart;
     const char *BufferPtr;
 
 public:
-    Lexer(const llvm::StringRef &Buffer)
-    {
+    explicit Lexer(const llvm::StringRef &Buffer) {
         BufferStart = Buffer.begin();
         BufferPtr = BufferStart;
     }
+    
     void next(Token &token);
 
 private:
     void formToken(Token &Result, const char *TokEnd, Token::TokenKind Kind);
+    void skipMultiLineComment();
 };
 
 #endif
